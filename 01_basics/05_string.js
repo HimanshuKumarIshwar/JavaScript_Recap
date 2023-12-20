fadf43b1897ca47a4715556512303d325061af17
Himanshu Kumar <himanshuishwar399@gmail.com>
2023-12-20 21:13:45 +0530

    Leran string and its method

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name ="himanshu"
+const repoCount =50
+//console.log(name + repoCount+ " Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String('hiteshc')
+
+// console.log(gameName[0])
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// console.log(name.length)
+  
+console.log(gameName.charAt[0])
+console.log(gameName.substring(0,5))
+console.log(gameName.slice(0,5))
+
+
+
+
+
