be637912488bdea6ae16c3f1c26074977b3decbd
Himanshu Kumar <himanshuishwar399@gmail.com>
2023-12-21 20:28:04 +0530

    number Maths and dateTime

---
+++ 01_basics/06_nums_Maths.js
@@ -0,0 +1,41 @@
+const score = 400
+
+
+const balance =new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(score.toString().length)
+
+// console.log(score.toFixed(2))
+
+
+// const otherNumber =23.456
+
+// console.log(otherNumber.toPrecision(2))
+
+
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.56))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.8));
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min =10;
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
