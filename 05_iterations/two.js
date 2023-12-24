5bca3db48e32d93d8695062b6c31ee8c9c177209
Himanshu Kumar <himanshuishwar399@gmail.com>
2023-12-24 20:47:46 +0530

    Loop for loop while loop and do while loop and break and continue

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+
+let index =0
+// while (index <= 10) {
+//     console.log(`Value of Index is ${index}`);
+//     index =index+2
+// }
+
+
+let myArray = ["flash", "Batman", "superman"]
+let arr =0
+while (arr < myArray.length) {
+   // console.log(`Value of array is ${myArray[arr]}`);
+    arr++
+}
+let score = 11
+do {
+     console.log(`Score is ${score}`);
+     score++
+} while (score<=10);
